dc8f6465af76133c5fde6e73ad9b1e8edd15f092
Maniek <matios13@gmail.com>
2018-01-24 15:15:21 +0100

    animacje  super duper

--- Assets/Scripts/MoveState.cs
+++ Assets/Scripts/MoveState.cs
@@ -44,6 +44,9 @@ public class MoveState : State<OrcAI>
 	public override void UpdateState(OrcAI _owner)
 	{
 		var deltaX = _owner.transform.localPosition.x - _owner.playerMovement.localPosition.x;
+		if (_owner.orc.currentHealth < 1) {
+			_owner.stateMachine.ChangeState (DeadState.Instance);
+		}
 		//Debug.Log(deltaX);
 		if (deltaX < -4)
 		{
@@ -58,7 +61,7 @@ public class MoveState : State<OrcAI>
 		{
 			_owner.movement.Move(0, false, 0);
 		}
-		if (deltaX > 50)
+		if (deltaX > 20)
 		{
 			_owner.stateMachine.ChangeState(WaitState.Instance);
 		} 
@@ -67,7 +70,14 @@ public class MoveState : State<OrcAI>
 			if (_owner.shouldAttack)
 			{
 				_owner.shouldAttack = false;
-				_owner.stateMachine.ChangeState(AttackState.Instance);				
+				if (_owner.orc.currentHealth < 50) 
+				{
+					_owner.cooldown = 4;
+					_owner.stateMachine.ChangeState (Attack2State.Instance);		
+				} else 
+				{
+					_owner.stateMachine.ChangeState (AttackState.Instance);				
+				}
 			}
 		}
 	}
